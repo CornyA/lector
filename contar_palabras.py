f1324a3207350327e111db39ef6fcff4b65b17bd
Cassiopea <josealbertonr99@gmail.com>
2020-02-08 00:45:51 +0000

    Defini funcion main y palabras(cambiará de nombre)

--- contar_palabras.py
+++ contar_palabras.py
@@ -4,17 +4,19 @@ import argparse
 
 def palabras(archivo):
     texto = lector.leer_archivo(archivo)
+    diccionario = lector.contar_palabras(texto)
+    
 
-def main(archivo):
-    print(palabras("/tmp/frankenstein.txt")
+
+def main(ahrchivo):
+    print()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-a", "--archivo", dest="archivo", help="Directorio del archivo", required=True)
-    #parser.add_argument("-m", "--minimo", dest="minimo", help="Minimo de repeticiones", required=False, default=2, type=int)
     parser.add_argument("-s", "--stopwords", dest= "stopwords", help="Directorio de StopWords", required=False)
     args = parser.parse_args()
     archivo = args.archivo
-    #minimo = args.minimo
     sw = args.stopwords
     main(archivo)
