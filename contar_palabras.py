965f0b907ac9d8aaff18f4f1ac042bbce563214a
Cassiopea <josealbertonr99@gmail.com>
2020-02-13 00:46:46 +0000

    Metodo de porcentaje_palabras_stop contaba letras en el texto no palabras, cree nuevo metodo porcentaje_palabras que ahora si lee las palabras en el texto e hice mi codigo un poco mas legible

--- contar_palabras.py
+++ contar_palabras.py
@@ -2,17 +2,51 @@
 import lector
 import argparse
 
+'''
 def porcentaje_palabras_stop(archivo):
     texto = lector.leer_archivo(archivo)
     diccionario = lector.contar_palabras(texto)
-    tam_texto = len(texto)
+    lista_palab = texto.split(" ")
+    tam_texto = len(lista_palab)
     tam_dip = contar_repeticiones(diccionario)
-    tam_stop = tam_texto - tam_dip
+    stop_words = lector.leer_stopwords("/home/cassiopea/josenoriega/spanish_stopwords.txt")
+    tam_stop = len(stop_words)
     porc_stop = (tam_stop*100)/tam_texto
-    porc_clave = 100-porc_stop
+    porc_clave = (tam_dip*100)/tam_texto
     print("Palabras en el texto: ",tam_texto,"| Porcentaje = 100%")
     print("Palabras stop:        ",tam_stop,"| Porcentaje = ",porc_stop, "%")
     print("Diccionario(clave):   ",tam_dip,"| Porcentaje = ",porc_clave, "%")
+'''    
+    
+def porcentaje_palabras(archivo,set_stop):
+    texto = lector.leer_archivo(archivo)
+    lista_palabras = texto.split(" ")
+    tam_texto = len(lista_palabras) #Numero de palabras
+    
+    stopwords = lector.leer_stopwords(set_stop)
+    dpc = dict()
+    dps = dict()
+    
+    for palabra in lista_palabras:
+        p = palabra.lower().strip(".,")
+        if p in stopwords:
+            if p in dps:
+                dps[p] += 1
+            else:
+                dps[p] = 1
+        else:
+            if p in dpc:
+                dpc[p] += 1
+            else:
+                dpc[p] = 1
+                
+    sumapc = contar_repeticiones(dpc)
+    sumaps = contar_repeticiones(dps)
+    porc_stop = (sumaps*100)/tam_texto
+    porc_clave = (sumapc*100)/tam_texto
+    print("Palabras en el texto: ",tam_texto,"| Porcentaje = 100%")
+    print("Palabras clave:       ",sumapc,"| Porcentaje = ",porc_clave, "%")
+    print("Palabras stop:        ",sumaps,"| Porcentaje = ",porc_stop, "%")
     
     
 def contar_repeticiones(diccionario):
@@ -29,7 +63,7 @@ def palabras_unicas(archivo):
     set_clave = set()
     set_stop = set()
     for palabra in lista_palabras:
-        p = palabra.strip(",.")
+        p = palabra.lower().strip(",.")
         if p in set_stopw:
             set_stop.add(p)
         else:
@@ -49,7 +83,8 @@ def palabras_unicas(archivo):
 
 def main(archivo):
     print("----------------Porcentaje de palabras-------------")
-    porcentaje = porcentaje_palabras_stop(archivo)
+    #porcentaje = porcentaje_palabras_stop(archivo)
+    porcentaje_palabras(archivo,"/home/cassiopea/josenoriega/spanish_stopwords.txt")
     print("---------------Porcentaje de palabras unicas-------")
     palabras_unicas(archivo)
 
