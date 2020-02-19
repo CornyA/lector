1dd9981a7a3057619454aa8a4b9fa6bd63d34a1d
Cassiopea <josealbertonr99@gmail.com>
2020-02-19 00:44:27 +0000

    Añadi funcion para obtener las palabras aleatorias del texto y funcion para limpiar la lista de palabras (quitarle newlines, puntos y comas).

---
+++ palabras_aleatorias.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import lector
+import argparse
+import random
+
+def obtener_palabras_aleatorias(archivo,num_pal):
+    texto = lector.leer_archivo(archivo)
+    lista_palabras = texto.split(" ")
+    lista_palabras = remover_espacios(lista_palabras)
+    lista_aleatoria = []
+    tam_texto = len(lista_palabras)
+    for i in range(0,num_pal):
+        rand = random.randint(0,tam_texto-1)
+        palabra = lista_palabras[rand]
+        lista_aleatoria.append(palabra)
+    return lista_aleatoria
+    
+def remover_espacios(lista_palabras):
+    lista_limpia = [palabra.rstrip(",.") for palabra in lista_palabras if len(palabra)>0]
+    return lista_limpia
+
+def main(archivo,num_pal):
+    lista_aleatoria = obtener_palabras_aleatorias(archivo,num_pal)
+    print(lista_aleatoria)
+    
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--archivo", dest="archivo", help="nombre de archivo", required=True)
+    parser.add_argument("-n", "--numpal", dest="numpal", help="Numero de palabras aleatorias", required=False, default=3, type=int)
+    args = parser.parse_args()
+    archivo = args.archivo
+    numpal = args.numpal
+    main(archivo,numpal)
+    
