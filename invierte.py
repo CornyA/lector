fba10a24f0f1b06a353337536bc064932d45e4be
Cassiopea <josealbertonr99@gmail.com>
2020-02-21 00:44:57 +0000

    Se creo archivo invierte.py y se crearon dos metodos iniciales, que aunque funcionaban, no era la idea, así que se mejoro, adaptandolo más al lenguaje de Python (esta comentado el primer metodo que se 'pensó en otro idioma' y en funcionamiento el nuevo).

---
+++ invierte.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import argparse
+import lector
+
+'''"Pensado en otro idioma"
+def invertir(archivo):
+    texto = lector.leer_archivo(archivo)
+    lista_palabras = texto.split(" ")
+    tam_lista = len(lista_palabras)
+    lista_invertida = []
+    for i in range(tam_lista-1,-1,-1):
+        lista_invertida.append(lista_palabras[i])
+    return lista_invertida
+'''
+def invertir(archivo):
+    texto = lector.leer_archivo(archivo)
+    lista_palabras = texto.split(" ")
+    #tam_lista = len(lista_palabras)
+    lista_invertida = []
+    for palabra in lista_palabras:
+        lista_invertida.insert(0,palabra)
+    return lista_invertida
+
+def escribir_invertido(fichero,lista_invertida):
+    texto_invertido = " ".join(lista_invertida)
+    try:
+        with open(fichero,"w") as fw:
+            fw.write(texto_invertido)
+    except:
+        print("Hubo un error")
+
+def main(archivo,fichero):
+    lista_invertida = invertir(archivo)
+    escribir_invertido(fichero,lista_invertida)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a","--archivo", dest="archivo",help="Ruta de archivo",required=True)
+    parser.add_argument("-o","--output", dest="output",help="Nombre de fichero",required=True)
+    args = parser.parse_args()
+    archivo = args.archivo
+    fichero = args.output
+    main(archivo,fichero)
